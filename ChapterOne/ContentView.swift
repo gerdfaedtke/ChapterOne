cbb017447dde644daf3800c51f022774ef466a39
Gerd Faedtke <gerd.faedtke@icloud.com>
2024-01-18 14:22:22 +0100

    Chapter 1.18 Function

--- ChapterOne/ContentView.swift
+++ ChapterOne/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     
@@ -13,6 +14,9 @@ struct ContentView: View {
     @State private var imageName = ""
     @State private var lastMessageNumber = -1
     @State private var lastImageNumber = -1
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var soundName = ""
+    @State private var lastSoundNumber = -1
 
     var body: some View {
         
@@ -60,17 +64,42 @@ struct ContentView: View {
                 messageString = messages[messageNumber]
                 lastMessageNumber = messageNumber
                 
- //             imageName = "image\(Int.random(in: 0...9))"
+                //             imageName = "image\(Int.random(in: 0...9))"
                 
                 var imageNumber = Int.random(in: 0...9)
                 while imageNumber == lastImageNumber {
                     imageNumber = Int.random(in: 0...9)
                 }
                 imageName = "image\(imageNumber)"
+                
                 lastImageNumber = imageNumber
+            
+                var soundNumber: Int
+                repeat {
+                    soundNumber = Int.random(in: 0...5)
+                } while soundNumber == lastSoundNumber
+                    lastSoundNumber = soundNumber
+                let soundName = "sound\(soundNumber)"
+                
+                
+                   
+                guard let soundFile = NSDataAsset(name: soundName) else {
+                    print("😡 Could not read file name\(soundName)")
+                    return
+                }
+                do {
+                    audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                    audioPlayer.play()
+                } catch {
+                    print("😡 Error \(error.localizedDescription) creating audioPlayer")
+                }
+                
+                
+                
             }
+            .buttonStyle(.borderedProminent)
         }
-        .buttonStyle(.borderedProminent)
+        .padding()
     }
 }
 
